0cf45b40c52484a1bf6eeb706426d77e410a9dbc
Dmytro G. Sergiienko <sergiienko.d.g@gmail.com>
2015-10-19 16:04:06 +0300

    Initial version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-;(function() {
+;(function($) {
 
-})();
+	$(function() {
+		var $slider = $(".ba-slider");
+
+		$slider.slick({
+			dots: true
+		});
+
+		// Increase and decrease quantity.
+		var $qtyField = $(".ba-product__qty-field"),
+			$qtyUp = $(".ba-product__qty-up"),
+			$qtyDown = $(".ba-product__qty-down");
+
+		$qtyUp.on("click", function(e) {
+			e.preventDefault();
+			var oldVal = $qtyField.val();
+			$qtyField.val(++oldVal);
+		});
+
+		$qtyDown.on("click", function(e) {
+			e.preventDefault();
+			var oldVal = $qtyField.val();
+			$qtyField.val(--oldVal || 1);
+		});
+
+	});
+
+})(jQuery);
